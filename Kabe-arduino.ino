508ef24604d99f0128a4aa4f40a0bd9a45766e9c
ekusy <ptx014@gmail.com>
2016-02-05 15:47:05 +0900

    キャリブレーション用関数

--- Kabe-arduino.ino
+++ Kabe-arduino.ino
@@ -1,8 +1,8 @@
 /*
- 1または2にするとシリアルモニターでセンサーの値を確認できるモードになる
- 1でモータセンサ、2で圧力センサ
- 確認後は0に戻して書き込み直すように
- */
+  1または2にするとシリアルモニターでセンサーの値を確認できるモードになる
+  1でモータセンサ、2で圧力センサ
+  確認後は0に戻して書き込み直すように
+*/
 int testMode = 0;
 
 void setup() {
@@ -12,9 +12,14 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  sendValue();
-  readValue();
-  safeFunction();
+  if (testMode > 0) {
+    calibrationMode(testMode);
+  }
+  else {
+    sendValue();
+    readValue();
+    safeFunction();
+  }
 }
 
 
